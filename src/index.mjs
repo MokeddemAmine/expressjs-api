24ab3440987b23490662dd9d7d660bd49937298b
Amine Mokeddem <mokeddemamine1707@gmail.com>
2024-10-11 08:25:12 +0200

    get request with route params and query params

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,37 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const users = [
+    {id:1,username:'aminemokeddem'},
+    {id:2,username:'mohammed'},
+    {id:3,username:'nouri'}
+];
+
+app.get('/',(request,response) => {
+    response.status(201).send({msg:'amine mokeddem'})
+})
+
+app.get('/api/users',(request,response) => {
+    const {query:{filter,value}} = request;
+    if(filter && value){
+        return response.send(
+            users.filter((user) =>  user[filter].includes(value))
+        )
+    }
+   return response.status(201).send(users)
+})
+
+app.get('/api/users/:id',(request,response) => {
+    const id = parseInt(request.params.id);
+    if(isNaN(id)){
+        return response.status(400).send({msg:'Bad Request. Invalid ID'});
+    }
+    const getUser = users.find((user) => user.id === id);
+    if(!getUser)
+        return response.sendStatus(404);
+    return response.send(getUser);
+})
+
 app.listen(PORT,() => {
     console.log(`Running on Port ${PORT}`);
 })
